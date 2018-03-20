a9b5169a63a45258f43c7e8d1bfb6b2448284c3f
Jacqueline <leejjjacq@gmail.com>
2018-03-20 16:50:48 +1100

    First commit

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import UserList from '../containers/user-list';
-import UserDetails from '../containers/user-detail';
-require('../../scss/style.scss');
+import UserList from '../containers/user-list.js';
+import '../../scss/style.scss';
 
 const App = () => (
-    <div>
-        <h2>User List</h2>
-        <UserList />
-        <hr />
-        <h2>User Details</h2>
-        <UserDetails />
-    </div>
+  <div>
+    <h2>Red Velvet</h2>
+    <UserList />
+    <hr />
+    <h2>User Details:</h2>
+  </div>
 );
 
 export default App;
--- dev/js/reducers/reducer-users.js
+++ dev/js/reducers/reducer-users.js
@@ -1,33 +1,44 @@
-/*
- * The users reducer will always return an array of users no matter what
- * You need to return something, so if there are no users then just return an empty array
- * */
-
-export default function () {
-    return [
-        {
-            id: 1,
-            first: "Bucky",
-            last: "Roberts",
-            age: 71,
-            description: "Bucky is a React developer and YouTuber",
-            thumbnail: "http://i.imgur.com/7yUvePI.jpg"
-        },
-        {
-            id: 2,
-            first: "Joby",
-            last: "Wasilenko",
-            age: 27,
-            description: "Joby loves the Packers, cheese, and turtles.",
-            thumbnail: "http://i.imgur.com/52xRlm8.png"
-        },
-        {
-            id: 3,
-            first: "Madison",
-            last: "Williams",
-            age: 24,
-            description: "Madi likes her dog but it is really annoying.",
-            thumbnail: "http://i.imgur.com/4EMtxHB.png"
-        }
-    ]
+export default function() {
+  return [
+    {
+      id: 1,
+      first: "Irene",
+      last: "Bae",
+      age: 27,
+      description: "Goddess Leader and Visual of Famous Kpop Sensation Group, Red Velvet",
+      thumbnail: "https://i.pinimg.com/originals/33/22/9d/33229de65b8a9487a21f584662b3b97f.jpg"
+    },
+    {
+      id: 2,
+      first: "Seulgi",
+      last: "Kang",
+      age: 24,
+      description: "Dance Carry of Famous Kpop Sensation Group, Red Velvet",
+      thumbnail: "https://i.pinimg.com/736x/b7/e5/fd/b7e5fde7602f24b778e82aae765f3d5d.jpg"
+    },
+    {
+      id: 3,
+      first: "Wendy",
+      last: "Son",
+      age: 24,
+      description: "Power Vocal of Famous Kpop Sensation Group, Red Velvet",
+      thumbnail: "https://pbs.twimg.com/media/CvolrERVUAAWSZB.jpg"
+    },
+    {
+      id: 4,
+      first: "Joy",
+      last: "Park",
+      age: 22,
+      description: "Aegyo Master of Famous Kpop Sensation Group, Red Velvet",
+      thumbnail: "https://pm1.narvii.com/6186/e45bd39d9b7634c324bf00ddd23b8480cbb82c4e_hq.jpg"
+    },
+    {
+      id: 5,
+      first: "Yeri",
+      last: "Kim",
+      age: 19,
+      description: "Cheeky Maknae of Famous Kpop Sensation Group, Red Velvet",
+      thumbnail: "https://78.media.tumblr.com/5f6fd165abac9cac920f222dd80b7da9/tumblr_o49pdkmUlA1v1lpero1_250.jpg"
+    }
+  ]
 }
